73b0deb9e7bd7da6ab6c2430d23386f66e3e494f
piyushdude25 <piyushdude25@gmail.com>
2022-11-16 19:54:34 +0530

    changes

--- src/apis/Storeapi.jsx
+++ src/apis/Storeapi.jsx
@@ -1,13 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
 export default axios.create({
-    // baseURL: "https://meesho123.herokuapp.com",
-    // baseURL: "https://fakestoreapi.com",
-    // baseURL:" https://meeshoserver.herokuapp.com/",
-    // baseURL:" https://meesho-clone2.herokuapp.com/",
-    // baseURL:"https://frountdata.herokuapp.com/"
-    baseURL:"https://db-server-mesho.herokuapp.com/"
-    // baseURL:"http://localhost:8000/"
-
-})
-
+  baseURL: "https://db-server-mesho.herokuapp.com/",
+  // baseURL: "https://meesho123.herokuapp.com",
+  // baseURL: "https://fakestoreapi.com",
+  // baseURL:" https://meeshoserver.herokuapp.com/",
+  // baseURL:" https://meesho-clone2.herokuapp.com/",
+  // baseURL:"https://frountdata.herokuapp.com/"
+  // baseURL:"http://localhost:8000/"
+});
